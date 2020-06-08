b5ff565a2fcaa07bcf4360c70db157cdf594c443
sanyokmalyshev <63872907+sanyokmalyshev@users.noreply.github.com>
2020-06-08 15:15:37 +0300

    samples rxjs

---
+++ src/subjects.js
@@ -0,0 +1,13 @@
+import { Subject, BehaviorSubject, ReplaySubject} from 'rxjs';
+document.addEventListener('click', () => {
+
+    const stream$ = new ReplaySubject(2);
+
+    
+
+    stream$.next('Hello');
+    stream$.next('Rx');
+
+    stream$.subscribe(v => console.log('Value: ', v));
+    
+})
